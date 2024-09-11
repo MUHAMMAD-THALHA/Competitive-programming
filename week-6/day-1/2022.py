44feb5083c6b3a506e13c6f48ab0730a643b5834
MT200524 <muhammadthalha2409@gmail.com>
2024-09-11 16:59:03 +0530

    Done

---
+++ week-6/day-1/2022.py
@@ -0,0 +1,12 @@
+# Convert 1D array into 2D array.
+
+class Solution:
+    def construct2DArray(self, original: list[int], m: int, n: int) -> list[list[int]]:
+        if len(original) != m * n:
+            return []
+        
+        result = []
+        for i in range(m):
+            result.append(original[i * n:(i + 1) * n])
+        
+        return result
